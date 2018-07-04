92bf9721d32774e59df5a95b71556654158ad7c0
Adheip <adheip.nagarajan@gmail.com>
2018-07-04 07:05:38 -0400

    Stable commit on Views and Relevance

--- scraper.py
+++ scraper.py
@@ -13,12 +13,13 @@ driver = webdriver.Chrome(r"C:\Users\adhei\Anaconda2\selenium\webdriver\chrome\c
 driver.get(url_base+url_search)
 
 ##Scroll down page to load and get more results
-#elem = driver.find_element_by_tag_name("body")
-#no_of_pagedowns = 20
-#while no_of_pagedowns:
-#    elem.send_keys(Keys.PAGE_DOWN)
-#    time.sleep(0.2)
-#    no_of_pagedowns-=1
+elem = driver.find_element_by_tag_name("body")
+no_of_pagedowns = 20
+while no_of_pagedowns:
+    elem.send_keys(Keys.PAGE_DOWN)
+    time.sleep(0.2)
+    no_of_pagedowns-=1
+
 
 
 #Checks for topic relevance in 
@@ -26,42 +27,46 @@ def check_to_process():
     total_count = 0
     relevance_count = 0
     rel_tot = 0
-    video_titles = driver.find_elements_by_xpath("""//*[@id="video-title"]""")
-    for titles in video_titles:
+    video_views = []
+    video_title_elements = driver.find_elements_by_xpath("""//*[@id="video-title"]""")
+    for elements in video_title_elements:
         if total_count >= 10:
             break
-        rel_tot += relevant_topics(titles,relevance_count)
-        print("Rel to",rel_tot," Total Iterations:",total_count)
-        #title_string = titles.get_attribute('title')
-        video_views = titles.get_attribute('aria-label')
-        #print title_string
+        
+        #Check for relevance with related terms
+        rel_tot += relevant_topics(elements,relevance_count)
+        
+        #Check for views
+        video_views.append(check_views(elements)) 
+        
         total_count += 1
-        #if any(s in title_string for s in ("Dota 2","DOTA 2","Dota2","DOTA2")):
-        #if "Dota 2" or "DOTA 2" or "Dota2" or "DOTA2" in str:
-        #if "Dota 2" in title_string:
-            #relevance_count += 1
-    print rel_tot
     relevance_percent = (float(rel_tot)/total_count) * 100
-    print relevance_percent,
     if relevance_percent > 70:
+        for v in video_views:
+            print v
+            if v > 100000 or v < 10000:
+                print ("in False")
+                return False
         return True
     else:
         return False
-
-def relevant_topics(titles,relevance_count):
-    title_string = titles.get_attribute('title')
+    
+    
+def relevant_topics(title,relevance_count):
+    title_string = title.get_attribute('title')
     if any(s in title_string for s in ("Dota 2","DOTA 2","Dota2","DOTA2")):
         return 1
     else:
         return 0
         
-def check_views():
-    video_views = titles.get_attribute('aria-label')
-    video_views.split()
+def check_views(views):
+    video_views_raw = views.get_attribute('aria-label')
+    return (int(video_views_raw.split(' ')[-2].replace(",","")))
+
 
 ##Build indexed Xpaths and retrieve URL value from each in href attribute
 i=1
-while i < 10:
+while i < 6:
     channel_xpath ="""//*[@id="contents"]/ytd-channel-renderer["""+str(i)+"]/a"
     channel = driver.find_element_by_xpath(channel_xpath)
     print channel.get_attribute('href')
